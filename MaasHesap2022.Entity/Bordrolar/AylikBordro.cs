c1b0e250a58b03651acda6eb668ecd1701247cfa
engin can <engin_can1528@hotmail.com>
2022-01-31 20:57:54 +0300

    veri modeli güncellendi

---
+++ MaasHesap2022.Entity/Bordrolar/AylikBordro.cs
@@ -0,0 +1,76 @@
+﻿using MaasHesap2022.Entity.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaasHesap2022.Entity.Bordrolar
+{
+    public class AylikBordro : BaseEntity
+    {
+        /// <summary>
+        /// Personel tekil bilgisi
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Ay Bilgisi
+        /// </summary>
+        public string Ay { get; set; }
+
+        /// <summary>
+        /// BrutUcret
+        /// </summary>
+        public decimal BrutUcret { get; set; }
+
+        /// <summary>
+        /// Sgk
+        /// </summary>
+        public decimal SgkIsci { get; set; }
+        /// <summary>
+        /// İşsizlik
+        /// </summary>
+        public decimal IssizlikIsci { get; set; }
+        /// <summary>
+        /// Kumulatif gelir vergisi matrah
+        /// </summary>
+        public decimal KumulatifGelirVergisiMatrahi { get; set; }
+        /// <summary>
+        /// Gelir vergisi matrah
+        /// </summary>
+        public decimal GelirVergisiMatrahi { get; set; }
+        /// <summary>
+        /// Gelir vergisi
+        /// </summary>
+        public decimal GelirVergisi { get; set; }
+        /// <summary>
+        /// Damga vergisi
+        /// </summary>
+        public decimal DamgaVergisi { get; set; }
+
+        /// <summary>
+        /// Kesintiler toplamı
+        /// </summary>
+        public decimal KesintilerToplami { get; set; }
+
+        /// <summary>
+        /// Net ücret
+        /// </summary>
+        public decimal NetUcret { get; set; }
+
+        /// <summary>
+        /// Gelir vergisi istisna tutar
+        /// </summary>
+        public decimal GelirVergisiIstisnaTutar { get; set; }
+        
+        /// <summary>
+        /// Damga vergisi istisna tutar
+        /// </summary>
+        public decimal DamgaVergisiIstisnaTutar { get; set; }
+
+        /// <summary>
+        /// Net ücret
+        /// </summary>
+        public decimal NetEleGecenUcret { get; set; }
+
+    }
+}
---
+++ MaasHesap2022.Entity/Users/User.cs
@@ -0,0 +1,34 @@
+﻿using MaasHesap2022.Entity.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaasHesap2022.Entity.User
+{
+    /// <summary>
+    /// Personellerin tutulduğu tablodur.
+    /// </summary>
+    public class User : BaseEntity
+    {
+        /// <summary>
+        /// Personel adı
+        /// </summary>
+        public string Ad { get; set; }
+
+        /// <summary>
+        /// Personel Soyadı
+        /// </summary>
+        public string Soyad { get; set; }
+
+        /// <summary>
+        /// Evlilik durumu
+        /// </summary>
+        public bool IsEvli { get; set; }
+
+        /// <summary>
+        /// Çocuk sayısı
+        /// </summary>
+        public int CocukSayisi { get; set; }
+
+    }
+}
