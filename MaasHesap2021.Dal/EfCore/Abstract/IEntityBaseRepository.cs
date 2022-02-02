fd4cb8cd6c869b26f28870fb659c3b2c4e0e22a1
engin can <engin_can1528@hotmail.com>
2022-02-02 20:54:31 +0300

    base repositories added

---
+++ MaasHesap2021.Dal/EfCore/Abstract/IEntityBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MaasHesap2022.Dal.EfCore.Abstract
+{
+    public interface IEntityBaseRepository<TEntity> where TEntity : class, new()
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ MaasHesap2021.Dal/EfCore/Concrete/EntityBaseRepository.cs
@@ -0,0 +1,90 @@
+﻿using MaasHesap2022.Dal.EfCore.Abstract;
+using MaasHesap2022.Entity.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MaasHesap2022.Dal.EfCore.Concrete
+{
+    public class EntityBaseRepository<TEntity> : HttpContextAccessor, IEntityBaseRepository<TEntity> where TEntity : class, new()
+    {
+        private MaasHesapContext _context;
+        public EntityBaseRepository(MaasHesapContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Add(TEntity entity)
+        {
+
+            var addedEntity = _context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            BeforeSaving();
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+
+            var deletedEntity = _context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            BeforeSaving();
+            _context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().Where(filter).SingleOrDefault();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null ? _context.Set<TEntity>().ToList() : _context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Update(TEntity entity)
+        {
+            var updatedEntity = _context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            BeforeSaving();
+            _context.SaveChanges();
+
+        }
+
+        private void BeforeSaving()
+        {
+            if (HttpContext != null)
+            {
+                DateTime now = DateTime.Now;
+                int.TryParse(HttpContext?.User.Claims.FirstOrDefault(x => x.Type == "userId")?.Value, out int id);
+                int? userId = (id == 0 ? (int?)null : id);
+
+
+                foreach (var item in _context.ChangeTracker.Entries().Where(e => e.Entity is BaseEntity))
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Detached:
+                            ((BaseEntity)item.Entity).CreatedAt = now;
+                            break;
+                        case EntityState.Unchanged:
+                            break;
+                        case EntityState.Deleted:
+                            break;
+                        case EntityState.Modified:
+                            break;
+                        case EntityState.Added:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
